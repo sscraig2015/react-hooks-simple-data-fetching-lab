bbc9c27121083a85cec8c0d6baba9c851b82f587
Sean <craig.sean92@gmail.com>
2022-02-04 17:15:32 -0700

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
-// create your App component here
+import React, {  useState, useEffect} from "react"
+import RenderImage from "./RenderImage"
+
+function App(){
+    
+    const [fetchedImage, setFetchedImage] = useState('')
+    const [loading, setLoading] = useState(true)
+    
+    useEffect(()=> {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then((resp) => resp.json())
+            .then((data) => {
+                setFetchedImage(data.message)
+                setLoading(false)
+                
+            })
+            
+    },[])
+
+    return(
+        <div>
+            <p>{loading ? 'Loading...' : null}</p>
+            <RenderImage image={fetchedImage}/>
+        </div>
+    )
+
+}
+
+export default App
---
+++ src/components/RenderImage.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function RenderImage({image}){
+    return(
+        <img alt="A Random Dog" src={image} />
+    )    
+}
+
+export default RenderImage
